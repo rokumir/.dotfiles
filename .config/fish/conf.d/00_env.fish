3c719e280efd9e6fc2fb250ef6265dd5cc877edb
Lopt <lopt.loptr@proton.me>
2026-03-05 21:45:04 +0700

    :sparkles: add pam_env config

--- .config/fish/conf.d/00_env.fish
+++ .config/fish/conf.d/00_env.fish
@@ -1,11 +1,3 @@
-fish_add_path -g ~/.local/bin # third parties' scripts
-fish_add_path -g ~/.bun/bin ~/.cache/.bun/bin
-fish_add_path -g ~/.cargo/bin
-fish_add_path -g ~/.deno/bin
-fish_add_path -g $XDG_DATA_HOME/bob/nvim-bin
-fish_add_path -g $XDG_DATA_HOME/fnm
-fish_add_path -g ~/go/bin
-
 type -q fnm && fnm env --use-on-cd --shell=fish --version-file-strategy=recursive | source
 
 set -gx GIT_EDITOR nvim
---
+++ .config/pam/README.md
@@ -0,0 +1,4 @@
+> [!IMPORTANT] The file `pam_env.conf` has to be put in `/etc/security/pam_env.conf` location for the `pam_env` to read it.
+
+- Install `pam_env` if haven't exist.
+- Then run `sudo cp ~/.config/pam/pam_env.conf /etc/security/pam_env.conf`
