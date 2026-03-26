cd81ad070ab06dc7d1ad8cba493aac725feaa010
Lopt <lopt.loptr@proton.me>
2026-03-26 07:28:33 +0700

    :sparkles: update env

--- .config/env/README.md
+++ .config/env/README.md
@@ -1,15 +1,7 @@
 1. Copy the `xdg.sh` file over.
 
 ```fish
-sudo cp ~/.config/pam/xdg.sh /etc/profile.d/xdg.sh
+sudo cp ~/.config/env/xdg.sh /etc/profile.d/xdg.sh
 ```
 
 2. Logout and log back in.
-
-3. If still doesn't work, use the `pam_env.conf` file instead.
-
-```fish
-sudo cp ~/.config/pam/pam_env.conf /etc/security/pam_env.conf
-sudo chown root:root /etc/security/pam_env.conf
-sudo chmod 644 /etc/security/pam_env.conf
-```
--- .config/env/xdg.sh
+++ .config/env/xdg.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
-# XDG ENV
+# shellcheck disable=SC2155,SC1090
+
+### XDG ENV
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_STATE_HOME=$HOME/.local/state
 
-# Scripts
-export PATH=$PATH:$HOME/.local/bin
-export PATH=$PATH:$HOME/.scripts
-export PATH=$PATH:$HOME/.local/scripts
-export PATH=$PATH:$HOME/.bun/bin
-export PATH=$PATH:$HOME/.cargo/bin
-export PATH=$PATH:$HOME/.deno/bin
-export PATH=$PATH:$HOME/go/bin
-export PATH=$PATH:$XDG_DATA_HOME/bob/nvim-bin
-export PATH=$PATH:$XDG_DATA_HOME/fnm
+### MISC
+export TERMINAL=$(gsettings get org.gnome.desktop.default-applications.terminal exec | tr -d "'")
+
+### Paths
+addpath() {
+	if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+		export PATH="$1${PATH:+":$PATH"}"
+	fi
+}
+
+addpath "$HOME/.local/bin"
+addpath "$HOME/.scripts"
+addpath "$HOME/.local/scripts"
+addpath "$HOME/.bun/bin"
+addpath "$HOME/.cargo/bin"
+addpath "$HOME/.deno/bin"
+addpath "$HOME/go/bin"
+addpath "$XDG_DATA_HOME/bob/nvim-bin"
+addpath "$XDG_DATA_HOME/fnm"
